b2e6493d0974cfe3a3fb1102c44b2462c2aec097
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-21 12:26:21 +0530

    created the fields for person class and imported ArrayList class as we are using ArrayList.

--- src/PersonDetails/Person.java
+++ src/PersonDetails/Person.java
@@ -1,4 +1,11 @@
 package PersonDetails;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName = "";
+    private String lastName = "";
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private String email = "";
+
 }
