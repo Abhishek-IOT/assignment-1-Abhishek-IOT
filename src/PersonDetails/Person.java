bb3a7242ffa3158c928ff554212fc685505f18de
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-21 12:28:14 +0530

    created the getter and setter function

--- src/PersonDetails/Person.java
+++ src/PersonDetails/Person.java
@@ -8,6 +8,38 @@ public class Person {
     private ArrayList<String> arrayList = new ArrayList<>();
     private String email = "";
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<String> getArrayList() {
+        return arrayList;
+    }
+
+    public void setArrayList(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Person(String firstName, String lastName, ArrayList<String> arrayList, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
