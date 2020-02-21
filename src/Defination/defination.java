b0ab6916be0e79a43463968b2d6e8fbe6bc68a4a
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-21 13:38:18 +0530

    created the defination class and inheriting the adt class features for the use

---
+++ src/Defination/defination.java
@@ -0,0 +1,22 @@
+package Defination;
+
+import ADT.linkedListADT;
+
+public class defination<Person> implements linkedListADT<Person> {
+
+    public Person add(Person details) {
+        return null;
+    }
+
+    public Person delete(int index) {
+        return null;
+    }
+
+    public void search(String person) {
+
+    }
+
+    public void viewAllDetails() {
+
+    }
+}
