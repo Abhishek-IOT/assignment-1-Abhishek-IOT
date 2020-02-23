6c641c34a4b3126d50bac61c3586e98bfac6cf3d
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-23 13:06:11 +0530

    created the main class and called the object of defination class

---
+++ src/Execution/Main.java
@@ -0,0 +1,12 @@
+package Execution;
+
+import Defination.defination;
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+    defination def = new defination();
+
+
+}
